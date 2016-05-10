39c214a8fb00421227d9bc30f4d181033222c64a
WeiTChen <1396559921@qq.com>
2016-05-10 12:00:46 +0800

    Add files via upload

---
+++ NSURLSessionDownload-master/URLSession/URLSession/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  URLSession
+//
+//  Created by William on 16/4/26.
+//  Copyright © 2016年 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSURLSessionDownload-master/URLSession/URLSessionTests/URLSessionTests.m
@@ -0,0 +1,39 @@
+//
+//  URLSessionTests.m
+//  URLSessionTests
+//
+//  Created by William on 16/4/26.
+//  Copyright © 2016年 William. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface URLSessionTests : XCTestCase
+
+@end
+
+@implementation URLSessionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
